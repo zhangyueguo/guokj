10d1638f57cdba98e5dda83270f108b6942c1b51
1156391296@qq.com <1156391296@qq.com>
2016-09-05 23:55:53 +0800

    jiazai model

--- app/Controller/indexController.php
+++ app/Controller/indexController.php
@@ -6,6 +6,10 @@ class IndexController{
 	public function Index()
 	{
 		p('it is index');
+		$model = new \core\lib\model();
+		$sql = "select * from ceshi";
+		$res = $model->query($sql);
+		p($res->fetchAll());
 	}
 	
 	
